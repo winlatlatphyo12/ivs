4754921ce8d8b64f922a08935081bb66ca01e74b
Win Lat Lat Phyoe <winlatlatphyo12@gmail.com>
2022-10-26 10:41:45 +0630

    Design of ivs

---
+++ includes/nav.php
@@ -0,0 +1,49 @@
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+            <div class="container-fluid px-4 px-lg-5">
+        
+            <img class="rounded-circle img-fluid mt-4" src="assets/img/bcslogo.jpg" alt="Mobirise Website Builder" style="height: 5rem;">
+                        
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                            Menu
+                            <i class="fas fa-bars"></i>
+                        </button>
+                
+                
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ms-auto py-4 py-lg-0">
+                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.php">About Us</a></li>
+                     
+                
+                       
+                        <li class="nav-item dropdown">
+                            <!-- data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation" -->
+                            <a class="nav-link  dropdown-toggle px-lg-3 py-3 py-lg-4" href="#" data-bs-toggle="dropdown">  Venture  </a>
+                                <ul class="dropdown-menu dropdown-menu-dark text-white bg-opacity-50 animated fadeInDown">
+                                    <li><a class="dropdown-item text-light" href="healthcare.php"> Health Care </a></li>
+                                    <li><a class="dropdown-item text-light" href="engineering.php"> Engineering </a></li>
+                                    <li><a class="dropdown-item text-light" href="it.php"> IT </a></li>
+                                    <li><a class="dropdown-item text-light" href="business.php"> Business </a></li>
+                                    <li><a class="dropdown-item text-light" href="hotel&tourism.php"> Hotel & Tourism </a></li>
+                                    <li><a class="dropdown-item text-light" href="education.php"> Education </a></li>
+                                    <li><a class="dropdown-item text-light" href="general.php">General</a></li>
+                                </ul>
+                        </li>
+                       
+                        <li class="nav-item">
+                             <a class="nav-link px-lg-3 py-3 py-lg-4" href="faq.php">FAQ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-lg-3 py-3 py-lg-4" href="blog.php">Blog</a>
+                       </li>
+                       <li class="nav-item">
+                        <a class="nav-link px-lg-3 py-3 py-lg-4" href="career.php">Career</a>
+                   </li>
+                       <li class="nav-item">
+                        <a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.php">Contact Us</a>
+                   </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
---
+++ index2.php
@@ -0,0 +1,74 @@
+<?php
+        include "includes/head.php";
+        include "includes/nav.php";
+        include "includes/header.php";
+?>
+
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-3 mb-4">
+                <img class="rounded-circle img-fluid" alt="100x100" 
+                src="assets/img/ivs7.jpg"
+                data-holder-rendered="true">
+
+                <h4 class="text-center my-5 h4">Health Care</h4>
+            </div>
+            <div class="col-md-3 mb-4">
+                <img class="rounded-circle img-fluid" alt="100x100" 
+                src="assets/img/ivs7.jpg"
+                data-holder-rendered="true">
+
+                <h4 class="text-center my-5 h4">Engineering</h4>
+            </div>
+            <div class="col-md-3 mb-4">
+                <img class="rounded-circle img-fluid" alt="100x100" 
+                src="assets/img/ivs7.jpg"
+                data-holder-rendered="true">
+
+                <h4 class="text-center my-5 h4">IT</h4>
+            </div>
+            <div class="col-md-3 mb-4">
+                <img class="rounded-circle img-fluid" alt="100x100" 
+                src="assets/img/ivs7.jpg"
+                data-holder-rendered="true">
+
+                <h4 class="text-center my-5 h4">Business</h4>
+            </div>
+        
+       
+            
+            <div class="col-md-3 mb-4">
+                <img class="rounded-circle img-fluid" alt="100x100" 
+                src="assets/img/ivs7.jpg"
+                data-holder-rendered="true">
+
+                <h4 class="text-center my-5 h4">Hotel & Tourism</h4>
+            </div>
+            <div class="col-md-3 mb-4">
+                <img class="rounded-circle img-fluid" alt="100x100" 
+                src="assets/img/ivs7.jpg"
+                data-holder-rendered="true">
+
+                <h4 class="text-center my-5 h4">Education</h4>
+            </div>
+            <div class="col-md-3 mb-4">
+                <img class="rounded-circle img-fluid" alt="100x100" 
+                src="assets/img/ivs7.jpg"
+                data-holder-rendered="true">
+
+                <h4 class="text-center my-5 h4">General</h4>
+            </div>
+        </div>
+    </div>
+   
+
+        <?php
+            include "includes/footer.php";
+        ?>
+      
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
