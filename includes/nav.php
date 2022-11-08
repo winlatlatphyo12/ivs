13df77a81cfd630ee334356a3f52e45afd36e585
Win Lat Lat Phyoe <winlatlatphyo12@gmail.com>
2022-11-08 14:29:24 +0630

    Design of ivs

--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,5 @@
 <!-- Navigation-->
-        <!-- <nav class="navbar navbar-fixed-top navbar-expand-lg px-4 px-lg-5" id="mainNav"> 
+        <nav class="navbar navbar-fixed-top navbar-expand-lg px-4 px-lg-5" id="mainNav"> 
         
             <div class="container-fluid px-4 px-lg-5">
         
@@ -18,71 +18,8 @@
                      
                 
                        
-                        <li class="nav-item dropdown"> -->
-                            <!-- data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation" -->
-                            <!-- <a class="nav-link  dropdown-toggle px-lg-3 py-4 py-lg-4" href="#" data-bs-toggle="dropdown">  Venture  </a>
-                                <ul class="dropdown-menu dropdown-menu-dark text-white bg-opacity-50 animated fadeInDown">
-                                    <li><a class="dropdown-item text-light" href="healthcare.php"> Health Care </a></li>
-                                    <li><a class="dropdown-item text-light" href="engineering.php"> Engineering </a></li>
-                                    <li><a class="dropdown-item text-light" href="it.php"> IT </a></li>
-                                    <li><a class="dropdown-item text-light" href="business.php"> Business </a></li>
-                                    <li><a class="dropdown-item text-light" href="hotel&tourism.php"> Hotel & Tourism </a></li>
-                                    <li><a class="dropdown-item text-light" href="education.php"> Education </a></li>
-                                    <li><a class="dropdown-item text-light" href="general.php">General</a></li>
-                                </ul>
-                        </li>
-                       
-                        <li class="nav-item">
-                             <a class="nav-link px-lg-3 py-4 py-lg-4" href="faq.php">FAQ</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link px-lg-3 py-4 py-lg-4" href="blog.php">Blog</a>
-                       </li>
-                       <li class="nav-item">
-                        <a class="nav-link px-lg-4 py-3 py-lg-4" href="career.php">Career</a>
-                   </li>
-                       <li class="nav-item">
-                        <a class="nav-link px-lg-4 py-3 py-lg-4" href="contact.php">Contact Us</a>
-                   </li>
-                    </ul>
-                </div>
-            </div>
-        </nav> -->
-
-        
-        
-     <section data-bs-version="5.1" class="menu menu1 cid-t7FjXCLwoF" once="menu" id="menu1-0">
-        <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
-        <div class="container-fluid">
-            <div class="navbar-brand">
-                <span class="navbar-logo">
-                    <a href="">
-                        <img src="assets/img/IVS LOGO.png" alt="Mobirise Website Builder" style="wight: 6rem; height: 6rem;">
-                    </a>
-                </span>
-
-            </div>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse"
-                data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <div class="hamburger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </button>
-           
-                
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-4 py-lg-4" href="index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-4 py-lg-4" href="about.php">About Us</a></li>
-                     
-                
-                       
-                        <li class="nav-item dropdown">
-                            <!-- data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation" -->
+                        <li class="nav-item dropdown"> 
+                            <!--  data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation"  -->
                             <a class="nav-link  dropdown-toggle px-lg-3 py-4 py-lg-4" href="#" data-bs-toggle="dropdown">  Venture  </a>
                                 <ul class="dropdown-menu dropdown-menu-dark text-white bg-opacity-50 animated fadeInDown">
                                     <li><a class="dropdown-item text-light" href="healthcare.php"> Health Care </a></li>
@@ -103,14 +40,15 @@
                        </li>
                        <li class="nav-item">
                         <a class="nav-link px-lg-4 py-3 py-lg-4" href="career.php">Career</a>
-                       </li>
+                   </li>
                        <li class="nav-item">
                         <a class="nav-link px-lg-4 py-3 py-lg-4" href="contact.php">Contact Us</a>
-                       </li>
+                   </li>
                     </ul>
                 </div>
-            
-        </div>
-    </nav>
-</section> 
+            </div>
+        </nav>
 
+        
+        
+    
--- index.php
+++ index.php
@@ -1,28 +1,12 @@
 <?php
         include "includes/head.php";
+        include "includes/header.php";
         include "includes/nav.php";
 ?>
 
-<section data-bs-version="5.1" class="header6 cid-t7Fp4iG4Hi mbr-fullscreen mbr-parallax-background" id="header6-1" style="background-image:url('header_img.png') !important;">
 
 
-
-
-    <div class="mbr-overlay" style="opacity: 0.3; background-color: rgb(68, 121, 217);"></div>
-
-    <div class="align-center container">
-        <div class="row justify-content-center">
-            <div class="col-12 col-lg-10">
-                <h1 class="mbr-section-title mbr-fonts-style mbr-white mb-3 display-2"><strong>Welcome to International School Rangoon</strong></h1>
-
-
-                
-            </div>
-        </div>
-    </div>
-</section>
-
-    <div class="container px-4 px-lg-5">
+    <div class="container p-4 px-lg-5" style="background-color: #f19f21;">
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-3 mb-4">
                 <img class="rounded-circle img-fluid" alt="100x100" 
