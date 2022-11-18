2f7d5f6abf6f474f9ad5e12329315e539e60cdc7
Kaung Htun Thant <3199busi@gmail.com>
2022-11-18 16:18:10 +0630

    Header text fixed

--- includes/header.php
+++ includes/header.php
@@ -2,10 +2,10 @@
 <header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
             <div class="container position-relative px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
+                    <div class="col-md-10 col-lg-8 col-xl-7 w-100">
                         <div class="site-heading">
-                            <h1 class="text-warning">IVS</h1>
-                            <span class="subheading">- Institute of Vocational Studies</span>
+                            <p class="h2"><span class="text-warning h1"><b>IVS</b></span> - Institute of Vocational Studies</p>
+                            
                             <span class="heading">"Ideal Place for Your Career Growth"</span>
                         </div>
                     </div>
--- index.php
+++ index.php
@@ -7,62 +7,6 @@
 
 
     <div class="container p-4 px-lg-5">
-        <!-- <div class="row gx-4 gx-lg-5 justify-content-center">
-            <div class="col-md-3 mb-4">
-                <img class="rounded-circle img-fluid" alt="100x100" 
-                src="assets/img/ivs7.jpg"
-                data-holder-rendered="true">
-
-                <h4 class="text-center my-5 h4">Health Care</h4>
-                
-            </div>
-            <div class="col-md-3 mb-4">
-                <img class="rounded-circle img-fluid" alt="100x100" 
-                src="assets/img/ivs7.jpg"
-                data-holder-rendered="true">
-
-                <h4 class="text-center my-5 h4">Engineering</h4>
-            </div>
-            <div class="col-md-3 mb-4">
-                <img class="rounded-circle img-fluid" alt="100x100" 
-                src="assets/img/ivs7.jpg"
-                data-holder-rendered="true">
-
-                <h4 class="text-center my-5 h4">IT</h4>
-            </div>
-            <div class="col-md-3 mb-4">
-                <img class="rounded-circle img-fluid" alt="100x100" 
-                src="assets/img/ivs7.jpg"
-                data-holder-rendered="true">
-
-                <h4 class="text-center my-5 h4">Business</h4>
-            </div>
-        
-       
-            
-            <div class="col-md-3 mb-4">
-                <img class="rounded-circle img-fluid" alt="100x100" 
-                src="assets/img/ivs7.jpg"
-                data-holder-rendered="true">
-
-                <h4 class="text-center my-5 h4">Hotel & Tourism</h4>
-            </div>
-            <div class="col-md-3 mb-4">
-                <img class="rounded-circle img-fluid" alt="100x100" 
-                src="assets/img/ivs7.jpg"
-                data-holder-rendered="true">
-
-                <h4 class="text-center my-5 h4">Education</h4>
-            </div>
-            <div class="col-md-3 mb-4">
-                <img class="rounded-circle img-fluid" alt="100x100" 
-                src="assets/img/ivs7.jpg"
-                data-holder-rendered="true">
-
-                <h4 class="text-center my-5 h4">General</h4>
-            </div>
-        </div> -->
-
         <div class="row gx-4 gx-lg-5">
             <div class="col-md-6 mb-4">
                 <img class="rounded-circle img-fluid" alt="100x100" 
