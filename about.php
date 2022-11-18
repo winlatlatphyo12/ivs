c3161026ccdfa6373c0dfe2a649852b0a5a8b113
Win Lat Lat Phyoe <winlatlatphyo12@gmail.com>
2022-11-18 16:23:14 +0630

    Footer Finalized and About us Aligned

--- about.php
+++ about.php
@@ -8,8 +8,8 @@
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                 
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                       <h2> What is IVS?</h2>
+                    <div class="col-md-12">
+                       <h2 class="text-center"> What is IVS?</h2>
                     </div>
                 </div>
                 <br>
--- includes/footer.php
+++ includes/footer.php
@@ -1,36 +1,52 @@
   <!-- Footer-->
   <footer class="border-top">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                        <ul class="list-inline text-center">
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-youtube fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-linkedin fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="small text-center text-muted fst-italic"> Powered By KBTC. <a href="https://kbtc.edu.mm/" class="link-primary text-decoration-underline">See More Info</a></div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-9">
+                <h3><u> Contact Info</u></h3>
+                <br>
+                
+                <div class="row">
+                    <div class="col-1">
+                        <p class="my-2"><i class="fa fa-envelope" aria-hidden="true"></i></p>
                     </div>
+                    <div class="col-11">
+                        <p class="my-2"> ivsinfo@ivs.com.mm</p>
+                    </div>
+                    <div class="col-1">
+                        <p class="my-2"><i class="fa fa-phone" aria-hidden="true"></i></p>
+                    </div>
+                    <div class="col-11">
+                        <p class="my-2"> +959 9777 487333​ </p>
+                    </div>                                
+                    <div class="col-1">
+                        <p class="my-2"><i class="fa fa-map-marker" aria-hidden="true"></i></p>
+                    </div>
+                    <div class="col-11">
+                        <p class="my-2"> Campus (1) - No (207), Pyay Road, ​Sanchaung Township, Yangon.​ <br> Campus (2) - Diamond Condo, No (1) G1, Pyay Road.​Kamayut Township, Yangon.​</p> 
+                    </div>                                
                 </div>
             </div>
-        </footer>
+            <div class="col-3 text-center">
+                <h3><u> Social Media </u></h3><br>
+                <span class="fa-stack fa-lg">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-youtube fa-stack-1x fa-inverse"></i>
+                </span>
+                <span class="fa-stack fa-lg">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                </span>
+                <span class="fa-stack fa-lg">
+                    <i class="fas fa-circle fa-stack-2x"></i>
+                    <i class="fab fa-linkedin fa-stack-1x fa-inverse"></i>
+                </span>
+                <br>
+                <br>
+                <br>
+                <button type="submit" class="btn btn-warning" name="contact"> Contact us </button>
+            </div>
+            <div class="col-12 small text-center text-muted fst-italic mt-5"> Powered By KBTC. <a href="https://kbtc.edu.mm/" class="link-primary text-decoration-underline">See More Info</a></div>
+        </div>
+    </div>
+</footer>
