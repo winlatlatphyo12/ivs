623383d9215c8d33b6e6356b166d6adbccf8480b
Kaung Htun Thant <3199busi@gmail.com>
2022-11-18 16:55:31 +0630

    Minor adjustment

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,16 @@
 <!-- Page Header-->
 <header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
-            <div class="container position-relative px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7 w-100">
-                        <div class="site-heading">
-                            <p class="h2"><span class="text-warning h1"><b>IVS</b></span> - Institute of Vocational Studies</p>
-                            
-                            <span class="heading">"Ideal Place for Your Career Growth"</span>
-                        </div>
-                    </div>
+    <div class="container position-relative px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7 w-100">
+                <div class="site-heading">
+                    <p class="h2"><span class="text-warning h1"><b>IVS</b></span> - Institute of Vocational Studies</p>
+                    
+                    <span class="heading">"Ideal Place for Your Career Growth"</span>
                 </div>
             </div>
-        </header>
+        </div>
+    </div>
+</header>
 
    
