7bfdbd8c7844af1516f9ef9395d228e42d722edd
Kaung Htun Thant <3199busi@gmail.com>
2022-11-21 10:57:11 +0630

    FAQ updated and minor color changes

--- faq.php
+++ faq.php
@@ -8,13 +8,90 @@
         <main class="mb-4">
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
-                <h2 class="text-center"> FAQ </h2>
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>
+                    <div class="col-sm-2 mb-4">
+                        <img class="rounded-circle img-fluid" alt="100x100" src="assets/img/ivs7.jpg" data-holder-rendered="true">
+                    </div>
+                    <div class="col-sm-8">
+                        <h5> Question </h5>
+                        <p> Lorem ipsum dolor sit amet consectetur? </p>
+                    </div>
+                    <div class="col-sm-2">
+                        
                     </div>
                 </div>
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-sm-2">
+                        
+                    </div>
+                    <div class="col-sm-8 text-end">
+                        <h5> Answer </h5>
+                        <p> Tenetur, ut, at nulla, nihil consequatur adipisci 
+                            officia asperiores magnam mollitia cupiditate inventore. Quo corporis 
+                            veniam assumenda earum ad? </p>
+                    </div>
+                    <div class="col-sm-2 mb-4 text-end">
+                        <img class="rounded-circle img-fluid" alt="100x100" src="assets/img/ivs7.jpg" data-holder-rendered="true">
+                    </div>
+                </div>
+                <hr>
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-sm-2 mb-4">
+                        <img class="rounded-circle img-fluid" alt="100x100" src="assets/img/ivs7.jpg" data-holder-rendered="true">
+                    </div>
+                    <div class="col-sm-8">
+                        <h5> Question </h5>
+                        <p> Lorem ipsum dolor sit amet consectetur? </p>
+                    </div>
+                    <div class="col-sm-2">
+                        
+                    </div>
+                </div>
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-sm-2">
+                        
+                    </div>
+                    <div class="col-sm-8 text-end">
+                        <h5> Answer </h5>
+                        <p> Tenetur, ut, at nulla, nihil consequatur adipisci 
+                            officia asperiores magnam mollitia cupiditate inventore. Quo corporis 
+                            veniam assumenda earum ad? </p>
+                    </div>
+                    <div class="col-sm-2 mb-4 text-end">
+                        <img class="rounded-circle img-fluid" alt="100x100" src="assets/img/ivs7.jpg" data-holder-rendered="true">
+                    </div>
+                </div>
+                <hr>
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-sm-2 mb-4">
+                        <img class="rounded-circle img-fluid" alt="100x100" src="assets/img/ivs7.jpg" data-holder-rendered="true">
+                    </div>
+                    <div class="col-sm-8">
+                        <h5> Question </h5>
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+                            neque praesentium. Tenetur, ut, at nulla, nihil consequatur adipisci 
+                            officia asperiores magnam mollitia cupiditate inventore. Quo corporis 
+                            veniam assumenda earum ad? </p>
+                    </div>
+                    <div class="col-sm-2">
+                        
+                    </div>
+                </div>
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-sm-2">
+                        
+                    </div>
+                    <div class="col-sm-8 text-end">
+                        <h5> Answer </h5>
+                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+                            neque praesentium. Tenetur, ut, at nulla, nihil consequatur adipisci 
+                            officia asperiores magnam mollitia cupiditate inventore. Quo corporis 
+                            veniam assumenda earum ad? </p>
+                    </div>
+                    <div class="col-sm-2 mb-4 text-end">
+                        <img class="rounded-circle img-fluid" alt="100x100" src="assets/img/ivs7.jpg" data-holder-rendered="true">
+                    </div>
+                </div>
+                <hr>
             </div>
         </main>
         
--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7 w-100">
                 <div class="site-heading">
-                    <p class="h2"><span class="text-warning h1"><b>IVS</b></span> - Institute of Vocational Studies</p>
+                    <p class="h2"><span class="text-secondary h1"><b>IVS</b></span> - Institute of Vocational Studies</p>
                     
                     <span class="heading">"Ideal Place for Your Career Growth"</span>
                 </div>
