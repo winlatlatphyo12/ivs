0769f99d26200772b082ef581dfbcdb0d73c8764
Win Lat Lat Phyoe <winlatlatphyo12@gmail.com>
2022-11-29 16:50:59 +0630

    Footer page

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
   <footer class="border-top bg-primary text-secondary">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-9 px-5">
+            <!-- <div class="col-md-9 px-5">
                 <h3 class="d-sm-none d-md-block"><u> Contact Info</u></h3>
                 <h3 class="d-md-none d-sm-block text-center"><u> Contact Info</u></h3>
                 <br>
@@ -27,42 +27,94 @@
                         <p class="my-2"> Campus (1) - No. 207, Pyay Road, ​Sanchaung Township, Yangon.​ <br> Campus (2) - Diamond Condo, No. 1 G1, Pyay Road.​Kamayut Township, Yangon.​</p> 
                     </div>                                
                 </div>
+            </div> -->
+            
+            <div class="col-md-6 px-5">
+                <h3 class="d-sm-none d-md-block fs-5"><u> Contact Info</u></h3>
+                <h3 class="d-md-none d-sm-block text-center" style="width: 20px; height: 20px;"><u> Contact Info</u></h3>
+                <br>
+        
+                <div class="row text-light mt-0">
+                    <div class="col-1">
+                        <p class="my-2"><i class="fa fa-envelope" aria-hidden="true"></i></p>
+                    </div>
+                    <div class="col-11">
+                        <p class="my-2 fs-6"> ivsinfo@ivs.com.mm</p>
+                    </div>
+                    <div class="col-1">
+                        <p class="my-2"><i class="fa fa-phone" aria-hidden="true"></i></p>
+                    </div>
+                    <div class="col-11">
+                        <p class="my-2 fs-6"> +959 9777 487333</p>
+                    </div>                                
+                    <div class="col-1">
+                        <p class="my-2"><i class="fa fa-map-marker" aria-hidden="true"></i></p>
+                    </div>
+                    <div class="col-11">
+                        <p class="my-2 fs-6"> Campus (1) - No. 207, Pyay Road, ​Sanchaung Township, Yangon.​ <br> Campus (2) - Diamond Condo, No. 1 G1, Pyay Road.​Kamayut Township, Yangon.​</p> 
+                    </div>                                
+                </div>
             </div>
 
-            <div class="col-md-3 text-center text-secondary">
+            <div class="col-md-2 px-5">
+                <h3 class="d-sm-none d-md-block fs-5"><u> Links</u></h3>
+                <h3 class="d-md-none d-sm-block text-center"><u> Links</u></h3>
+                    <div class="row mt-4">
+                        <div class="col-12">
+                            <p class="my-2 fs-6"><a href="index.php" class="text-light"> Home </a></p>
+                        </div>
+                        <div class="col-12">
+                            <p class="my-2 fs-6"><a href="activities.php" class="text-light"> Activities </a></p>
+                        </div>
+                        <div class="col-12">
+                            <p class="my-2 fs-6"><a href="about.php" class="text-light"> About IVS </a></p>
+                        </div>
+                        <div class="col-12">
+                            <p class="my-2 fs-6"><a href="contact.php" class="text-light"> Contact Us </a></p>
+                        </div>
+                        
+                    </div>
+            </div>
+
+
+            <div class="col-md-4 text-center text-secondary">
                 <br class="d-md-none">
                 <br class="d-md-none">
-                <h3 class="text-secondary"><u> Follow us on </u></h3><br>
-                <span class="fa-stack fa-lg">
+                <h3 class="text-secondary fs-5"><u> Follow us on </u></h3><br>
+                <span class="fa-stack fa-lg mt-4">
                     <a href="https://www.youtube.com/@ivs-mm" class="text-secondary">
-                        <i class="fas fa-circle fa-stack-2x"></i>
-                        <i class="fab fa-youtube fa-stack-1x fa-inverse"></i>
+                        <i class="fas fa-circle fa-stack-2x text-light"></i>
+                        <i class="fab fa-youtube fa-stack-1x fa-inverse text-primary"></i>
                     </a>
                 </span>
-                <span class="fa-stack fa-lg">
+                <span class="fa-stack fa-lg mt-4">
                     <a href="https://www.facebook.com/ivsmm​" class="text-secondary">
-                        <i class="fas fa-circle fa-stack-2x"></i>
-                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                        <i class="fas fa-circle fa-stack-2x text-light"></i>
+                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse text-primary"></i>
                     </a>
                 </span>
-                <span class="fa-stack fa-lg">
+                <span class="fa-stack fa-lg mt-4">
                     <a href="https://www.linkedin.com/company/ivsmm" class="text-secondary">
-                        <i class="fas fa-circle fa-stack-2x"></i>
-                        <i class="fab fa-linkedin fa-stack-1x fa-inverse"></i>
+                        <i class="fas fa-circle fa-stack-2x text-light"></i>
+                        <i class="fab fa-linkedin fa-stack-1x fa-inverse text-primary"></i>
                     </a>
                 </span>
                 <br>
                 <br>
+                <div class="col-sm-12 copyright fst-italic text-center fs-6 text-light"> Powered By KBTC. <a href="https://kbtc.edu.mm/" class="link-primary text-decoration-underline text-secondary">See More Info</a>
+                </div>
                 <br>
-                <a href="contact.php"><button type="submit" class="btn btn-secondary text-light" name="contact"> Contact us </button></a>
+                <br>
+                <br>
+                <!-- <a href="contact.php"><button type="submit" class="btn btn-secondary text-light" name="contact"> Contact us </button></a> -->
             </div>
         </div>
         
-        <div class="col-sm-12 copyright fst-italic text-end mt-5"> Powered By KBTC. <a href="https://kbtc.edu.mm/" class="link-primary text-decoration-underline">See More Info</a>
-            </div>
+        <!-- <div class="col-sm-12 copyright fst-italic text-end"> Powered By KBTC. <a href="https://kbtc.edu.mm/" class="link-primary text-decoration-underline">See More Info</a>
+            </div> -->
             <div class="col-sm-12 copyright fst-italic text-center mt-3">
                 <p class="h6 mbr-text mbr-fonts-style mbr-white text-muted">
-                    © Copyright 2022 Institute of Vocational Studies
+                    © 2022 Copyright Institute of Vocational Studies
                 </p>
             </div>
             
