825a51c8e0384cc4ddffdfdf0c4372720b3ae82c
Win Lat Lat Phyoe <winlatlatphyo12@gmail.com>
2023-01-10 13:16:04 +0630

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -5,90 +5,78 @@
       <!--Grid row-->
       <div class="row">
         <!--Grid column-->
-        <div class="col-lg-5 col-md-12 mb-4 mb-md-0">
+        <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
           <h6 class="text-uppercase mb-4">About company</h6>
 
-          <p class="fs-6">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-            voluptatum deleniti atque corrupti.
-          </p>
+            <p class="fs-6">
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+              voluptatum deleniti atque corrupti.
+            </p>
 
-          <p class="fs-6">
-            Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
-            molestias.
-          </p>
-
-            
-        
+            <p class="fs-6">
+              Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
+              molestias.
+            </p>
         </div>
         <!--Grid column-->
 
         <!--Grid column-->
-        <div class="col-lg-5 col-md-6 mb-4 mb-md-0">
+        <div class="col-lg-6 col-md-6 mb-4 mb-md-0">
           <h6 class="text-uppercase mb-4 pb-1">Contact Info</h6>
-
-        
-          <ul class="fa-ul" style="margin-left: 1.65em;">
-            <li class="mb-3 fs-6">
-              <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2">Campus (1) - No. 207, Pyay Road,<br>
-                &nbsp; ​Sanchaung Township, Yangon.​ <br> &nbsp;&nbsp;Campus (2) - Diamond Condo, No. 1 G1, Pyay Road,
-                 <br> &nbsp; ​Kamayut Township, Yangon.​</span>
-            </li>
-            <li class="mb-3 fs-6">
-              <span class="fa-li"><i class="fas fa-envelope"></i></span><span class="ms-2">ivsinfo@ivs.com.mm</span>
-            </li>
-            <li class="mb-3 fs-6">
-              <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2">+959 9777 487333</span>
-            </li>
-          
-          </ul>
+            <ul class="fa-ul" style="margin-left: 1.65em;">
+              <li class="mb-3 fs-6">
+                <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2">Campus (1) - No. 207, Pyay Road, ​Sanchaung Township, Yangon.​ <br> &nbsp;&nbsp;Campus (2) - Diamond Condo, No. 1 G1, Pyay Road, ​Kamayut <br> &nbsp;&nbsp;Township, Yangon.​</span>
+              </li>
+              <li class="mb-3 fs-6">
+                <span class="fa-li"><i class="fas fa-envelope"></i></span><span class="ms-2">ivsinfo@ivs.com.mm</span>
+              </li>
+              <li class="mb-3 fs-6">
+                <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2">+959 9777 487333</span>
+              </li>
+            </ul>
         </div>
         <!--Grid column-->
+    </div>
+        <!--Grid row-->
 
-        <!--Grid column-->
-        <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
-        
-              <h6 class="text-uppercase mb-3">Links</h6>
-                <div class="row mt-0">
-                    <div class="col-12">
-                        <p class="my-2 fs-6"><a href="index.php" class="text-light"> Home </a></p>
-                    </div>
-                    <div class="col-12">
-                        <p class="my-2 fs-6"><a href="activities.php" class="text-light"> Activities </a></p>
-                    </div>
-                    <div class="col-12">
-                        <p class="my-2 fs-6"><a href="about.php" class="text-light"> About IVS </a></p>
-                    </div>
-                    <div class="col-12">
-                        <p class="my-2 fs-6"><a href="contact.php" class="text-light"> Contact Us </a></p>
-                    </div>
+        <br><br>
+      <!--Grid row-->
+      <div class="row">
+          <div class="col-sm-9">
+                <!-- <h3 class="d-sm-none d-md-block fs-5"> Links</h3> -->
+                <!-- <h3 class="d-md-none d-sm-block text-center"><u> Links</u></h3> -->
+              
+                        <p class="my-2 fs-6"><a href="index.php" class="text-light"> Home </a>&nbsp;&nbsp;
+                        <a href="activities.php" class="text-light"> Activities </a>&nbsp;&nbsp;
+                        <a href="about.php" class="text-light"> About IVS </a>&nbsp;&nbsp;
+                        <a href="contact.php" class="text-light"> Contact Us </a></p>&nbsp;&nbsp;
                     
-                </div><br><br><hr>
-                <div class="row">
-                  <div class="col-lg-4 fa-stack fa-lg">
-                      <a href="https://www.youtube.com/@ivs-mm">
-                        <i class="fab fa-youtube fa-stack-1x fa-inverse text-light"></i>
-                      </a>
-                  </div>
-                  <div class="col-lg-4 fa-stack fa-lg">
-                          <a href="https://www.facebook.com/ivsmm​">
-                            <i class="fab fa-facebook-f fa-stack-1x fa-inverse text-light"></i>
-                          </a>
-                  </div>
-                  <div class="col-lg-4 fa-stack fa-lg">
-                          <a href="https://www.linkedin.com/company/ivsmm">
-                              <i class="fab fa-linkedin fa-stack-1x fa-inverse text-light"></i>
-                          </a>
-                  </div>
-                </div>
-        
+          </div>
+
+        <div class="col-sm-3">
+          <div class="row">
+              <div class="col-sm-4 fa-stack fa-lg">
+                  <a href="https://www.youtube.com/@ivs-mm">
+                      <i class="fab fa-youtube fa-stack-1x fa-inverse text-light"></i>
+                  </a>
+              </div>
+              <div class="col-sm-4 fa-stack fa-lg">
+                  <a href="https://www.facebook.com/ivsmm​">
+                      <i class="fab fa-facebook-f fa-stack-1x fa-inverse text-light"></i>
+                  </a>
+              </div>
+              <div class="col-sm-4 fa-stack fa-lg">
+                  <a href="https://www.linkedin.com/company/ivsmm">
+                      <i class="fab fa-linkedin fa-stack-1x fa-inverse text-light"></i>
+                  </a>
+              </div>
+          </div>
         </div>
-        <!--Grid column-->
       </div>
       <!--Grid row-->
     </div>
-    <br>
-    <br>
+    
+    
     <!-- Grid container -->
 
     <!-- Copyright -->
