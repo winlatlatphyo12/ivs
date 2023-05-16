50b75b2b4f56486336004c1dbfce690fed110577
Win Lae Lae Phyoe <winlatlatphyo12@gmail.com>
2023-05-16 11:49:11 +0630

    google analytics

--- includes/head.php
+++ includes/head.php
@@ -33,6 +33,18 @@
         <link href="css/styles.css" rel="stylesheet" />
 
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-X53N8C5MKS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-X53N8C5MKS');
+</script>
+
+
+
 
     </head>
     <body>
